a8afec85a117c55467782e56ea554feeeff2fa70
JunZheng.Liu <liujunzheng@ittlr.com>
2020-10-13 00:05:18 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# vue-codemirror
+
+> 基于  vue-codemirror-lite 的代码编辑器demo，支持动态批量
+
+
+## Building
+
+[![Build Status](https://travis-ci.org/yaimeet/vue-codemirror.svg?branch=master)](https://travis-ci.org/yaimeet/vue-codemirror)
+
+
+## Demo
+
+[https://5e02c0ea4e46f04cd0664f75--suspicious-poincare-8439cb.netlify.com/](https://5e02c0ea4e46f04cd0664f75--suspicious-poincare-8439cb.netlify.com/)
+
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <button @click='onAddCodeItem'>继续新增</button>
+    <div class="code-mirr-item" :key='codeIndex' v-for='(codeItem, codeIndex) in codeContentList'>
+      <codemirror
+        class='codemirror-item'
+        :options='options'
+        v-model='codeItem.value'></codemirror>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  codemirror
+} from 'vue-codemirror-lite'
+require('codemirror/mode/shell/shell')
+require('codemirror/theme/material-darker.css')
+export default {
+  name: 'app',
+  data () {
+    return {
+      codeContentList: [
+        {
+          value: '#!/usr/bin/bash\n'
+        },
+        {
+          value: '#!/usr/bin/bash\necho "test" \n'
+        }
+      ],
+      options: {
+        mode: 'shell',
+        theme: 'material-darker',
+        lineNumbers: true,
+        lineWrapping: true
+      }
+    }
+  },
+  components: {
+    codemirror
+  },
+  methods: {
+    onAddCodeItem () {
+      this.codeContentList.push({
+        value: ''
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.code-mirr-item{
+  border: 1px solid #ddd;
+  height: 100px;
+}
+.vue-codemirror-wrap,
+.cm-s-material-darker.CodeMirror{
+  height: 100%;
+}
+</style>
